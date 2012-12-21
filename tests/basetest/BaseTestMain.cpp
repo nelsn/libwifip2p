46a92ae8dfaaeb4ad4432a815aad80d286e73caf
Niels_W <n.wuerzbach@tu-bs.de>
2012-12-21 22:58:57 +0100

    Written down more of chapter 2, Working tilte: "Stand der Technik".

--- tests/basetest/BaseTestMain.cpp
+++ tests/basetest/BaseTestMain.cpp
@@ -2,5 +2,12 @@
 
 int main() {
 	wifip2p::SupplicantHandle handle("/var/run/wpa_supplicant");
+
+	handle.ctrlAttach("/var/run/wpa_supplicant");
+/*
+ * TODO qredqe
+ */
+	std::cout << "blabla" << std::endl;
+
 	return 0;
 }
